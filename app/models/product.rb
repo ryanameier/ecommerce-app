0f47c0499504c80b961dcd6752c9b0f1bf4f0042
Ryan Meier <rymei10@gmail.com>
2016-04-18 20:28:45 -0700

    added models changes into product.rb to help dry the code in controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,22 @@
 class Product < ActiveRecord::Base
+
+
+  def sale_message
+   if price <= 2
+     return "Discount item!"
+   elsif price > 2
+    return "On sale!"
+ end
 end
+
+ def tax
+  tax = price * 0.09
+  return "#{tax} for tax"
+ end
+
+ def total
+  total = price * 1.09
+  return total.to_i
+end
+end
+
